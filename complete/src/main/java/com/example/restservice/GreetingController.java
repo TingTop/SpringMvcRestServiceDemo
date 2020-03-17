e8bb7ec7b6c1a20e1d58a46094b56510e7874cd7
shiqingshan <sqskg@163.com>
2020-03-17 14:39:14 +0800

    init

---
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.restservice;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@GetMapping("/greeting2")
+	public Greeting greeting(@RequestBody Map<String,Object> params){
+		String productId = params.get("productId").toString();
+		return new Greeting(counter.incrementAndGet(), String.format(template, productId));
+	}
+}
