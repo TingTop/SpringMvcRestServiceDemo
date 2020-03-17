0c455e20c3aaafb904c8fa6bce3cb4c2696772d0
shiqingshan <sqskg@163.com>
2020-03-17 15:46:47 +0800

    request formate

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -5,6 +5,8 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +21,25 @@ public class GreetingController {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
+	/**
+	 * raw json
+	 * @param params
+	 * @return
+	 */
 	@GetMapping("/greeting2")
 	public Greeting greeting(@RequestBody Map<String,Object> params){
 		String productId = params.get("productId").toString();
 		return new Greeting(counter.incrementAndGet(), String.format(template, productId));
+	}
+
+	/**
+	 * form-data / x-www-form-urlencoded
+	 * @param firstName
+	 * @param lastName
+	 * @return
+	 */
+	@RequestMapping(value = "/greeting3",method = RequestMethod.POST)
+	public Greeting greeting(String firstName,String lastName){
+		return new Greeting(counter.incrementAndGet(),String.format(template,firstName + lastName));
 	}
 }
